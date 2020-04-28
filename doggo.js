fc34720ca94d8da2c1746363c1bbe275356b9d8a
Jack Skiles <jackskiles420@gmail.com>
2020-04-28 11:12:06 -0400

    updates

--- doggo.js
+++ doggo.js
@@ -3,23 +3,16 @@ const button = document.getElementById("dog");
 
 const body = document.getElementById("bod");
 
-const cont = document.getElementById("picCont");
 
 function buttonPress() {
     button.innerHTML = "Generating doggo.."
     $.get('https://dog.ceo/api/breeds/image/random')
     .then(function (data) {
+        const cont = document.getElementById("picCont");
         const img = document.createElement('img');
-        img.setAttribute = ('src', data.message);
+        img.setAttribute('src', data.message);
         cont.appendChild(img);
     })
-    //     console.error(data);
-    //     console.log(data);
-    //     console.info(data);
-    // })
-    // .catch(function(error) {
-        //     console.error(error);
-        // })
 }
 
 button.addEventListener('click', buttonPress);
